56f4aa49e308574fb790e750ea88c0453252947a
markmakave <markmakave@gmail.com>
2023-02-20 16:12:04 +0300

    .

--- hertz-web/html/script.js
+++ hertz-web/html/script.js
@@ -1,3 +1,153 @@
+class Camera {
+    constructor(canvas) {
+        this.canvas = canvas;
+        this.canvas.addEventListener("mousedown", this.onMouseDown.bind(this));
+        this.canvas.addEventListener("mousemove", this.onMouseMove.bind(this));
+        this.canvas.addEventListener("wheel", this.onMouseWheel.bind(this));
+        this.canvas.addEventListener("keydown", this.onKeyDown.bind(this));
+        this.canvas.addEventListener("keyup", this.onKeyUp.bind(this));
+        this.canvas.addEventListener("contextmenu", (e) => e.preventDefault());
+        this.canvas.focus();
+
+        this.mouse = {
+            locked: false
+        };
+        this.keys = {};
+
+        this.position = vec3.fromValues(0, 0, 0);
+        this.forward = vec3.fromValues(0, 0, 1);
+        this.right = vec3.fromValues(-1, 0, 0);
+        this.up = vec3.fromValues(0, 1, 0);
+
+        this.zoom = 1;
+
+        this.update();
+    }
+
+    update() {
+        this.canvas.width = this.canvas.clientWidth;
+        this.canvas.height = this.canvas.clientHeight;
+
+        this.aspect = this.canvas.width / this.canvas.height;
+
+        if (this.keys["w"]) {
+            this.moveForward();
+        }
+        if (this.keys["s"]) {
+            this.moveBackward();
+        }
+        if (this.keys["a"]) {
+            this.moveLeft();
+        }
+        if (this.keys["d"]) {
+            this.moveRight();
+        }
+        if (this.keys[" "]) {
+            this.moveUp();
+        }
+        if (this.keys["Shift"]) {
+            this.moveDown();
+        }
+    }
+
+    onMouseDown(e) {
+        if (e.button == 0) {
+            if (this.mouse.locked) {
+                this.mouse.locked = false;
+                // unlock cursor
+                document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+                document.exitPointerLock();
+
+                // show cursor
+                this.canvas.style.cursor = "auto";
+            } else {
+                this.mouse.locked = true;
+                // lock cursor
+                this.canvas.requestPointerLock = this.canvas.requestPointerLock || this.canvas.mozRequestPointerLock;
+                this.canvas.requestPointerLock();
+
+                // hide cursor
+                this.canvas.style.cursor = "none";
+            }
+        }
+    }
+
+    onMouseMove(e) {
+        if (this.mouse.locked) {
+            let dx = -e.movementX;
+            let dy = -e.movementY;
+
+            let sensitivity = 0.002
+
+            // rotate forward vector around up vector by dx
+            let q = quat.create();
+            quat.setAxisAngle(q, this.up, dx * sensitivity);
+            vec3.transformQuat(this.forward, this.forward, q);
+
+            // rotate forward vector around right vector by dy
+            quat.setAxisAngle(q, this.right, dy * sensitivity);
+            vec3.transformQuat(this.forward, this.forward, q);
+
+            // update right vector
+            vec3.cross(this.right, this.forward, this.up);
+            vec3.normalize(this.right, this.right);
+
+            // update forward vector
+            vec3.normalize(this.forward, this.forward);
+        }
+    }
+
+    onMouseWheel(e) {
+        this.zoom /= Math.pow(1.001, e.deltaY);
+    }
+
+    onKeyUp(e) {
+        this.keys[e.key] = false;
+    }
+
+    onKeyDown(e) {
+        this.keys[e.key] = true;
+    }
+
+    moveForward() {
+        vec3.add(this.position, this.position, this.forward);
+    }
+
+    moveBackward() {
+        vec3.sub(this.position, this.position, this.forward);
+    }
+
+    moveLeft() {
+        vec3.sub(this.position, this.position, this.right);
+    }
+
+    moveRight() {
+        vec3.add(this.position, this.position, this.right);
+    }
+
+    moveUp() {
+        vec3.add(this.position, this.position, this.up);
+    }
+
+    moveDown() {
+        vec3.sub(this.position, this.position, this.up);
+    }
+
+    getProjectionMatrix() {
+        let m = mat4.create();
+        mat4.perspective(m, 45, this.aspect, 0.1, 1000);
+        return m;
+    }
+
+    getViewMatrix() {
+        let m = mat4.create();
+        mat4.lookAt(m, this.position, vec3.add(vec3.create(), this.position, this.forward), this.up);
+        mat4.scale(m, m, vec3.fromValues(this.zoom, this.zoom, this.zoom));
+        return m;
+    }
+}
+
+
 class PointcloudRenderer {
     constructor() {
         this.canvas = document.getElementById("glCanvas");
@@ -6,8 +156,8 @@ class PointcloudRenderer {
         this.gl.clearColor(0.0, 0.0, 0.0, 0.8);
         this.gl.enable(this.gl.DEPTH_TEST);
         this.gl.depthFunc(this.gl.LEQUAL);
-        this.mode = this.gl.POINTS;
 
+        // compile vertex shader
         this.vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
         this.gl.shaderSource(this.vertexShader,
             `
@@ -24,6 +174,7 @@ class PointcloudRenderer {
         );
         this.gl.compileShader(this.vertexShader);
 
+        // compile fragment shader
         this.fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
         this.gl.shaderSource(this.fragmentShader, `
             precision mediump float;
@@ -34,86 +185,184 @@ class PointcloudRenderer {
         `);
         this.gl.compileShader(this.fragmentShader);
 
+        // link shaders into program
         this.program = this.gl.createProgram();
         this.gl.attachShader(this.program, this.vertexShader);
         this.gl.attachShader(this.program, this.fragmentShader);
         this.gl.linkProgram(this.program);
         this.gl.useProgram(this.program);
 
-        this.data = new Uint8Array(0);
-        this.pointCount = 0;
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        // payload
 
+        this.cloud = new Uint8Array(0);
+
+        this.cloudModel = mat4.create();
+        mat4.identity(this.cloudModel);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+        // gizmo
+
+        this.gizmo = new ArrayBuffer(16 * 6);
+
+        let gizmoData = new Float32Array(this.gizmo);
+
+        // X axis
+        gizmoData[0] = 0;
+        gizmoData[1] = 0;
+        gizmoData[2] = 0;
+        new Uint8Array(this.gizmo).set([255, 0, 0, 255], 12);
+
+        gizmoData[4] = 1;
+        gizmoData[5] = 0;
+        gizmoData[6] = 0;
+        new Uint8Array(this.gizmo).set([255, 0, 0, 255], 28);
+
+        // Y axis
+        gizmoData[8] = 0;
+        gizmoData[9] = 0;
+        gizmoData[10] = 0;
+        new Uint8Array(this.gizmo).set([0, 255, 0, 255], 44);
+
+        gizmoData[12] = 0;
+        gizmoData[13] = 1;
+        gizmoData[14] = 0;
+        new Uint8Array(this.gizmo).set([0, 255, 0, 255], 60);
+
+        // Z axis
+        gizmoData[16] = 0;
+        gizmoData[17] = 0;
+        gizmoData[18] = 0;
+        new Uint8Array(this.gizmo).set([0, 0, 255, 255], 76);
+
+        gizmoData[20] = 0;
+        gizmoData[21] = 0;
+        gizmoData[22] = 1;
+        new Uint8Array(this.gizmo).set([0, 0, 255, 255], 92);
+
+        this.gizmoModel = mat4.create();
+        mat4.identity(this.gizmoModel);
+        mat4.scale(this.gizmoModel, this.gizmoModel, [10, 10, 10]);
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        // buffers
+        this.gizmoBuffer = this.gl.createBuffer();
         this.pointBuffer = this.gl.createBuffer();
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pointBuffer);
-        let aPositionLocation = this.gl.getAttribLocation(this.program, "aPosition");
-        this.gl.enableVertexAttribArray(aPositionLocation);
-        this.gl.vertexAttribPointer(aPositionLocation, 3, this.gl.FLOAT, false, 16, 0);
-        let aColorLocation = this.gl.getAttribLocation(this.program, "aColor");
-        this.gl.enableVertexAttribArray(aColorLocation);
-        this.gl.vertexAttribPointer(aColorLocation, 4, this.gl.UNSIGNED_BYTE, true, 16, 12);
 
-        this.mvp = mat4.create();
-        this.mvpLocation = this.gl.getUniformLocation(this.program, "uMVP");
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
+        let camera = new Camera(this.canvas);
+
+        // set up event handlers
         window.onresize = () => {
             this.canvas.width = window.innerWidth;
             this.canvas.height = window.innerHeight;
             this.gl.viewport(0, 0, this.canvas.width, this.canvas.height);
-            mat4.lookAt(this.mvp, [0, 0, 0], [0, 0, -1], [0, 1, 0]);
-            mat4.perspective(this.mvp, 45 * Math.PI / 180, this.canvas.width / this.canvas.height, 0.1, 100000);
-            mat4.translate(this.mvp, this.mvp, [0, 0, -5]);
+            camera.update()
         };
         window.onresize();
-    }
 
-    render() {
-        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+        let render = () => {
+            this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
 
-        this.gl.uniformMatrix4fv(this.mvpLocation, false, this.mvp);
-        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pointBuffer);
+            camera.update();
 
-        this.gl.drawArrays(this.mode, 0, this.pointCount);
+            // move cloudModel back and forth along the Z axis using sin wave
+            mat4.identity(this.cloudModel);
 
+
+            {
+                // draw gizmo
+                this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.gizmoBuffer);
+                this.gl.bufferData(this.gl.ARRAY_BUFFER, this.gizmo, this.gl.STATIC_DRAW);
+
+                let aPosition = this.gl.getAttribLocation(this.program, "aPosition");
+                this.gl.enableVertexAttribArray(aPosition);
+                this.gl.vertexAttribPointer(aPosition, 3, this.gl.FLOAT, false, 16, 0);
+
+                let aColor = this.gl.getAttribLocation(this.program, "aColor");
+                this.gl.enableVertexAttribArray(aColor);
+                this.gl.vertexAttribPointer(aColor, 4, this.gl.UNSIGNED_BYTE, true, 16, 12);
+
+                let uMVP = this.gl.getUniformLocation(this.program, "uMVP");
+
+                let projection = camera.getProjectionMatrix();
+                let view = camera.getViewMatrix();
+
+                let mvp = mat4.create();
+                mat4.multiply(mvp, projection, view);
+                mat4.multiply(mvp, mvp, this.gizmoModel);
+
+                this.gl.uniformMatrix4fv(uMVP, false, mvp);
+
+                this.gl.drawArrays(this.gl.LINES, 0, 6);
+            }
+
+            {
+                // draw cloud
+                this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pointBuffer);
+                this.gl.bufferData(this.gl.ARRAY_BUFFER, this.cloud, this.gl.STATIC_DRAW);
+
+                let aPosition = this.gl.getAttribLocation(this.program, "aPosition");
+                this.gl.enableVertexAttribArray(aPosition);
+                this.gl.vertexAttribPointer(aPosition, 3, this.gl.FLOAT, false, 16, 0);
+
+                let aColor = this.gl.getAttribLocation(this.program, "aColor");
+                this.gl.enableVertexAttribArray(aColor);
+                this.gl.vertexAttribPointer(aColor, 4, this.gl.UNSIGNED_BYTE, true, 16, 12);
+
+                let uMVP = this.gl.getUniformLocation(this.program, "uMVP");
+
+                let projection = camera.getProjectionMatrix();
+                let view = camera.getViewMatrix();
+
+                let mvp = mat4.create();
+                mat4.multiply(mvp, projection, view);
+                mat4.multiply(mvp, mvp, this.cloudModel);
+
+                this.gl.uniformMatrix4fv(uMVP, false, mvp);
+
+                this.gl.drawArrays(this.gl.POINTS, 0, this.cloud.length / 16);
+            }
+            
+            requestAnimationFrame(render);
+        };
+
+        render();
     }
 
     addPoints(pointArray) {
-        let newData = new Uint8Array(this.data.length + pointArray.length);
-        newData.set(this.data);
-        newData.set(pointArray, this.data.length);
-        this.data = newData;
+        let newData = new Uint8Array(this.cloud.length + pointArray.length);
+        newData.set(this.cloud);
+        newData.set(pointArray, this.cloud.length);
+        this.cloud = newData;
 
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pointBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.data.buffer, this.gl.DYNAMIC_DRAW);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.cloud.buffer, this.gl.DYNAMIC_DRAW);
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
-        
-        this.pointCount = this.data.length / 16;
     }
 
     replacePoints(pointArray) {
-        this.data = pointArray;
+        this.cloud = pointArray;
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.pointBuffer);
-        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.data.buffer, this.gl.DYNAMIC_DRAW);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.cloud.buffer, this.gl.DYNAMIC_DRAW);
         this.gl.bindBuffer(this.gl.ARRAY_BUFFER, null);
-
-        this.pointCount = this.data.length / 16;
     }
 }
 
 const renderer = new PointcloudRenderer();
 let consoleBody = document.getElementById("consoleBody");
-let stereoFrame = document.getElementById("stereoFrame");
-
-stereoFrame.src = "images/na.png";
 
 let cache = new Uint8Array(0);
 
-function connectPWS() {
-    const pointWS = new WebSocket("wss://markmakave.com/points");
+function connectWS() {
+    const ws = new WebSocket("wss://markmakave.com/points");
 
     // accept binary data on connection
-    pointWS.binaryType = "arraybuffer";
+    ws.binaryType = "arraybuffer";
 
-    pointWS.onmessage = (event) => {
+    ws.onmessage = (event) => {
         // accumulate data until we have a full frame of 24*10000 bytes
         let data = new Uint8Array(event.data);
         let newData = new Uint8Array(cache.length + data.length);
@@ -121,19 +370,49 @@ function connectPWS() {
         newData.set(data, cache.length);
         cache = newData;
 
-        if (cache.length < 16*10000) {
+        if (cache.length < 10000) {
             return;
         }
 
-        let points = cache.slice(0, 16*10000);
-        cache = cache.slice(16*10000);
+        let frame = cache.slice(0, 10000);
+        cache = cache.slice(10000);
 
-        renderer.replacePoints(points);
+        let width = 100
+        let height = 100
+
+        let cloud = new ArrayBuffer(width * height * 16);
+        for (let y = 0; y < height; y++) {
+            for (let x = 0; x < width; x++) {
+                let index = (y * width + x);
+                let z = frame[index];
+
+                if (z == 255) {
+                    continue;
+                }
+
+                let r, g, b;
+
+                let factor = z / 255.0;
+                if (factor < 0.5) {
+                    r = 255 * (1 - factor * 2);
+                    g = 255 * (factor * 2);
+                    b = 0;
+                } else {
+                    r = 0;
+                    g = 255 * (1 - (factor - 0.5) * 2);
+                    b = 255 * ((factor - 0.5) * 2);
+                }
+
+                new Float32Array(cloud).set([width / 2 - x, height / 2 - y, z / 3], index * 4);
+                new Uint8Array(cloud).set([r, g, b, 255], index * 16 + 12);
+            }
+        }
+
+        renderer.replacePoints(new Uint8Array(cloud));
     }
 
-    pointWS.onopen = () => {
+    ws.onopen = () => {
         renderer.data = new Uint8Array(0);
-        renderer.pointCount = 0;
 
         let p = document.createElement("p");
         p.className = "info";
@@ -142,82 +421,16 @@ function connectPWS() {
         consoleBody.insertBefore(p, consoleBody.firstChild);
     }
 
-    pointWS.onclose = () => {
+    ws.onclose = () => {
+        renderer.data = new Uint8Array(0);
+
         let p = document.createElement("p");
         p.className = "error";
         p.innerText = "[ POINT ] Connection lost. Reconnecting...";
         consoleBody.insertBefore(p, consoleBody.firstChild);
-        connectPWS();
+        connectWS();
     }
     
 }
 
-function connectVWS() {
-    const videoWS = new WebSocket("wss://markmakave.com/video");
-
-    videoWS.willReadFrequently = true;
-    videoWS.binaryType = "arraybuffer";
-
-    videoWS.onmessage = (event) => {
-        let data = new Uint8Array(event.data);
-        let blob = new Blob([data], {type: "image/jpeg"});
-        stereoFrame.src = URL.createObjectURL(blob);
-    }
-
-    videoWS.onopen = () => {
-        let p = document.createElement("p");
-        p.className = "info";
-        p.innerText = "[ VIDEO ] Connected to video server.";
-        consoleBody.insertBefore(p, consoleBody.firstChild);
-    }
-
-    videoWS.onclose = () => {
-        let p = document.createElement("p");
-        p.className = "error";
-        p.innerText = "[ VIDEO ] Connection lost. Reconnecting...";
-        consoleBody.insertBefore(p, consoleBody.firstChild);
-        stereoFrame.src = "images/na.png";
-        connectVWS();
-    }
-}
-
-connectPWS();
-connectVWS();
-
-// switch renderer.mode on left click
-let mode = 0;
-document.onclick = () => {
-    mode = (mode + 1) % 3;
-    switch(mode) {
-        case 0:
-            renderer.mode = renderer.gl.POINTS;
-            break;
-        case 1:
-            renderer.mode = renderer.gl.LINE_STRIP;
-            break;
-        case 2:
-            renderer.mode = renderer.gl.TRIANGLES;
-            break;
-    }
-}
-
-while (true) {
-    let model = mat4.create();
-    let view = mat4.create();
-    let projection = mat4.create();
-    let mvp = mat4.create();
-
-    mat4.lookAt(view, [0, 0, -50], [0, 0, 0], [0, 1, 0]);
-    mat4.perspective(projection, 90 * Math.PI / 180, renderer.canvas.width / renderer.canvas.height, 0.1, 100000);
-
-    mat4.multiply(mvp, projection, view);
-    mat4.multiply(mvp, mvp, model);
-
-
-    
-    renderer.mvp = mvp;
-
-    renderer.render();
-
-    await new Promise(r => setTimeout(r, 1000 / 60));
-}
+connectWS();
--- hertz-web/server.js
+++ hertz-web/server.js
@@ -2,7 +2,6 @@ const https = require('https')
 const webSocket = require('ws')
 const net = require('net')
 const fs = require("fs")
-const jpegEncoder = require('jpeg-js')
 
 const options = {
     key: fs.readFileSync('ssl/privkey.pem'),
@@ -55,95 +54,28 @@ const webServer = https.createServer(options, (req, res) => {
     res.end()
 })
 
-var data = new Uint8Array(0)
-const pointWSS = new webSocket.Server({ noServer: true })
-pointWSS.on('connection', (socket) => {
-    console.log('[ POINT ] Client connected')
+const wss = new webSocket.Server({ noServer: true })
+wss.on('connection', (socket) => {
+    console.log('[ WEBSOCKET ] Client connected')
 
     socket.on('close', () => {
-        console.log('[ POINT ] Client disconnected')
+        console.log('[ WEBSOCKET ] Client disconnected')
     })
 })
 
-const videoWSS = new webSocket.Server({ noServer: true })
-videoWSS.on('connection', (socket) => {
-    console.log('[ VIDEO ] Client connected')    
-
-    socket.on('close', () => {
-        console.log('[ VIDEO ] Client disconnected')
-    })
-})
-
-const depthSS = net.createServer()
-depthSS.on('connection', (socket) => {
-    console.log('[ DEPTH ] Source connected')
-
-    var stream = new Uint8Array(0)
+const ss = net.createServer()
+ss.on('connection', (socket) => {
+    console.log('[ SOCKET ] Source connected')
 
     socket.on('data', (data) => {
-        let newData = new Uint8Array(stream.length + data.length)
-        newData.set(stream)
-        newData.set(data, stream.length)
-        stream = newData
-
-        if (stream.length < 10000) {
-            return
-        }
-
-        let frame = stream.slice(0, 10000)
-        stream = stream.slice(10000)
-
-        // convert grayscale image to pointcloud
-
-        let points = new ArrayBuffer(10000 * 16)
-        let width = 100
-        let height = 100
-
-        for (let i = 0; i < 10000; i++) {
-            let x = 50 - i % width
-            let y = 50 - Math.floor(i / width)
-            let z = frame[i]
-
-            if (z > 250){
-                z = 3.40282347e+38
-            }
-
-            z /= 3
-
-            let point = new ArrayBuffer(16)
-
-            new Float32Array(point).set([x, y, z, 1], 0)
-            new Uint8Array(point).set([255, 255, 255, 255], 12)
-
-            new Uint8Array(points).set(new Uint8Array(point), i * 16)
-        }
-        
-        // send points to client
-        pointWSS.clients.forEach((client) => {
-            client.send(points, { binary: true })
-        })
-        
-    })
-
-    socket.on('close', () => {
-        console.log('[ DEPTH ] Source disconnected')
-    })
-})
-
-const videoUSS = net.createServer()
-videoUSS.on('connection', (socket) => {
-    console.log('[ VIDEO ] Source connected')
-    
-    let frameCache = new Uint8Array(0)
-
-    socket.on('data', (data) => {
-        videoWSS.clients.forEach((client) => {
+        // forward all data to websocket clients
+	wss.clients.forEach((client) => {
             client.send(data, { binary: true })
         })
     })
 
     socket.on('close', () => {
-        console.log('[ VIDEO ] Source disconnected')
+        console.log('[ SOCKET ] Source disconnected')
     })
 })
 
@@ -155,20 +87,16 @@ webServer.listen(port, () => {
 })
 
 // listen depthSS on port 8081
-depthSS.listen(8081, () => {
-    console.log('[ DEPTH ] Listening on port 8081')
+ss.listen(8081, () => {
+    console.log('[ SOCKET ] Listening on port 8081')
 })
 
 webServer.on('upgrade', (req, socket, head) => {
     const pathname = req.url
 
     if (pathname === '/points') {
-        pointWSS.handleUpgrade(req, socket, head, (ws) => {
-            pointWSS.emit('connection', ws, req)
-        })
-    } else if (pathname === '/video') {
-        videoWSS.handleUpgrade(req, socket, head, (ws) => {
-            videoWSS.emit('connection', ws, req)
+        wss.handleUpgrade(req, socket, head, (ws) => {
+            wss.emit('connection', ws, req)
         })
     } else {
         socket.destroy()
