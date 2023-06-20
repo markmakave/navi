5c824737b56dec69bdce0aaacc8879a6d7203d23
markmakave <markmakave@gmail.com>
2023-06-20 11:23:55 +0300

    .

--- src/navi/main.cpp
+++ src/navi/main.cpp
@@ -11,12 +11,12 @@
 using namespace lumina;
 
 #define N 7
-#define ITERATIONS 10
+#define ITERATIONS 1
 
 int
-main()
+main(int argc, char** argv)
 {
-    image<rgb>      raw("../resource/stereo/photo.png");
+    image<rgb>      raw(argv[1]);
     tensor<3, byte> image(raw.shape()[0], raw.shape()[1], 3);
     for (int i = 0; i < raw.shape()[0]; ++i)
         for (int j = 0; j < raw.shape()[1]; ++j) {
@@ -29,13 +29,13 @@ main()
     d_image << image;
 
     cuda::tensor<3, float> d_kernel(3, 3, 1);
-    d_kernel(0, 0, 0) = 1;
-    d_kernel(1, 0, 0) = 1;
-    d_kernel(2, 0, 0) = 1;
+    d_kernel(0, 0, 0) = -1;
+    d_kernel(1, 0, 0) = -1;
+    d_kernel(2, 0, 0) = -1;
 
-    d_kernel(0, 1, 0) = 0;
-    d_kernel(1, 1, 0) = 0;
-    d_kernel(2, 1, 0) = 0;
+    d_kernel(0, 1, 0) = -1;
+    d_kernel(1, 1, 0) = 8;
+    d_kernel(2, 1, 0) = -1;
 
     d_kernel(0, 2, 0) = -1;
     d_kernel(1, 2, 0) = -1;
@@ -46,11 +46,10 @@ main()
 
     cuda::kernel convolve(cuda::convolve<3, byte, float, byte>);
 
-    auto convolve_launchable =
-        convolve.bind({image.shape()[0] / 8 + 1, image.shape()[1] / 8 + 1},
-                      {8, 8},
-                      cuda::stream::main,
-                      d_kernel.size() * sizeof(float));
+    auto convolve_launchable = convolve.bind({image.shape()[0] / 8 + 1, image.shape()[1] / 8 + 1},
+                                             {8, 8},
+                                             cuda::stream::main,
+                                             d_kernel.size() * sizeof(float));
 
     convolve_launchable(d_image, d_kernel, d_result);
     cuda::stream::main.synchronize();
